8fc4555b76654a7e370fdae4f65a1d8a95b3f92f
Shahid-Ayub <shahidayub5218@gmial.com>
2025-07-11 22:34:04 +0500

    Updated: extract_quotes_and_author_names.py

--- extract_quotes_and_author_names.py
+++ extract_quotes_and_author_names.py
@@ -1,23 +1,23 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup  
 
-url = input('Enter your URL: ')
+url = input('Enter your URL: ') # Says to users to enter the URL
 
-response = requests.get(url)
+response = requests.get(url)   # send request and get respone from the web
 
-if response.status_code == 200:
+if response.status_code == 200: # looks that url matches or not if yes it return 200 if no then 403
 
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    quotes = soup.find_all('div', class_='quote')
+    quotes = soup.find_all('div', class_='quote') # looks for the quotes
     
-    for i, quote in enumerate(quotes, start=1):
+    for i, quote in enumerate(quotes, start=1): # start the loop form 1
 
-        text = quote.find('span', class_='text').text
+        text = quote.find('span', class_='text').text # Make the quotes as same as at the web
+  
+        author = quote.find('small', class_='author').text 
 
-        author = quote.find('small', class_='author').text
-
-        print(f"{i}. {text} --- {author}")
+        print(f"{i}. {text} --- {author}")  # looks for the Author names who wrote the quotes
 
 else:
-    print("Failed to retrive the webpage", response.status_code)
+    print("Failed to retrive the webpage", response.status_code)  # if requests did not matches the response showa 404

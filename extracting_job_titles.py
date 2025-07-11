002a31a9cd9e2e73f5074228ca9bade527862bed
Shahid-Ayub <shahidayub5218@gmial.com>
2025-07-11 23:12:27 +0500

    explanation is in the comments

--- extracting_job_titles.py
+++ extracting_job_titles.py
@@ -1,23 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = input('Enter URL: ')
+url = input('Enter URL: ')  # ask's user to enter the URL
 
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
+response = requests.get(url) # SEND REQUEST TO THE WEV SERVER
 
-# Step 2: Find all job listings
-jobs = soup.find_all("div", class_="card-content")
+soup = BeautifulSoup(response.text, "html.parser") # look after the html content
 
-# Step 3: Filter and print jobs with "Python" in the title
+jobs = soup.find_all("div", class_="card-content") # this statments looks for all the jobs
+
 print("Python-related jobs:\n")
-for job in jobs:
-    title = job.find("h2", class_="title").text
-    company = job.find("h3", class_="company").text
-    location = job.find("p", class_="location").text
+ 
+for job in jobs: # loop for finding the required job details
+
+    title = job.find("h2", class_="title").text # loook for the job titles
+
+    company = job.find("h3", class_="company").text # look for the job company
+
+    location = job.find("p", class_="location").text # looks for the company location
 
     if "python" in title.lower():
-        print(f"Title: {title}")
+
+        print(f"Title: {title}")   # print the required data -----
+
         print(f"Company: {company}")
+
         print(f"Location: {location}")
+
         print("-" * 40)
